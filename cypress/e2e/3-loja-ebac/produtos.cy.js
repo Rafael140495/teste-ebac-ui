4cf85c39c0c15a1df6a9f6054a24db3b7d046b21
Rafixar <rafixar@gmail.com>
2025-05-21 12:02:32 -0300

    Seleção automática de produtos

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,16 @@
+///<reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+
+beforeEach(() => {
+    cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+});
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.products > .row')
+        .contains('Aero Daily Fitness Tee')
+        .click()
+
+        cy.get('#tab-title-description > a').should('contain', 'Descrição')
+    });
+});
